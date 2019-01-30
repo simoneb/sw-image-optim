130665795ee5cc95db14709abbae37819a26e7db
Simone Busoli <simone.busoli@gmail.com>
2019-01-30 15:39:24 +0100

    Delay service worker registration as per guidelines

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-navigator.serviceWorker.register(process.env.PUBLIC_URL + '/optimization-service-worker.js')
-.then(() => {
-  console.log('Registered service worker')
-  ReactDOM.render(<App />, document.getElementById('root'));
-})
-.catch(() => console.error('Could not register service worker'))
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register(process.env.PUBLIC_URL + '/optimization-service-worker.js')
+    .then(() => {
+      console.log('Registered service worker')
+    })
+    .catch(() => console.error('Could not register service worker'))  
+  })
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
